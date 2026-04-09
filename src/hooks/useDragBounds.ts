0bba42c88462e19d5793a361e18a1a7a534d1ed3
Biyu20 <434422935@qq.com>
2026-04-09 14:26:30 +0800

    feat(hooks): 新增useDragBounds hook

---
+++ src/hooks/useDragBounds.ts
@@ -0,0 +1,28 @@
+import { useRef, useState } from 'react';
+import { DraggableProps } from 'react-draggable';
+
+const useDragBounds = () => {
+  const dragRef = useRef<HTMLDivElement>(null);
+  const [bounds, setBounds] = useState({
+    left: 0,
+    top: 0,
+    bottom: 0,
+    right: 0,
+  });
+  const onStart: DraggableProps['onStart'] = (_event, uiData) => {
+    const { clientWidth, clientHeight } = window.document.documentElement;
+    const targetRect = dragRef.current?.getBoundingClientRect();
+    if (!targetRect) return;
+
+    setBounds({
+      left: -targetRect.left + uiData.x,
+      right: clientWidth - (targetRect.right - uiData.x),
+      top: -targetRect.top + uiData.y,
+      bottom: clientHeight - (targetRect.bottom - uiData.y),
+    });
+  };
+
+  return { dragRef, bounds, onStart };
+};
+
+export default useDragBounds;
